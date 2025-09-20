e422ba8a4138224a6e268bd36453f9619017a22e
Sachi Verma <your-email@example.com>
2025-09-21 00:23:10 +0530

    Flatten project for Vercel deployment

---
+++ backend/src/index.ts
@@ -0,0 +1,81 @@
+import "dotenv/config";
+import "./config/passport.config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import passport from "passport";
+import { Env } from "./config/env.config";
+import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { asyncHandler } from "./middlewares/asyncHandler.middlerware";
+import connctDatabase from "./config/database.config";
+import authRoutes from "./routes/auth.route";
+import { passportAuthenticateJwt } from "./config/passport.config";
+import userRoutes from "./routes/user.route";
+import transactionRoutes from "./routes/transaction.route";
+import { initializeCrons } from "./cron";
+import reportRoutes from "./routes/report.route";
+import analyticsRoutes from "./routes/analytics.route";
+
+const app = express();
+const BASE_PATH = Env.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
+
+app.use(
+  cors({
+    origin: Env.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+// ✅ Health check route
+app.get(`${BASE_PATH}/health`, (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running 🚀",
+    env: Env.NODE_ENV,
+  });
+});
+
+// ✅ Welcome root route
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Welcome to FintechAI API 🚀",
+    });
+  })
+);
+
+// ✅ API Routes
+app.use(`${BASE_PATH}/auth`, authRoutes);
+
+app.use(`${BASE_PATH}/user`, passportAuthenticateJwt, userRoutes);
+app.use(`${BASE_PATH}/transaction`, passportAuthenticateJwt, transactionRoutes);
+app.use(`${BASE_PATH}/report`, passportAuthenticateJwt, reportRoutes);
+app.use(`${BASE_PATH}/analytics`, passportAuthenticateJwt, analyticsRoutes);
+
+// ✅ 404 handler
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+// ✅ Global error handler
+app.use(errorHandler);
+
+app.listen(Env.PORT, async () => {
+  await connctDatabase();
+
+  if (Env.NODE_ENV === "development") {
+    await initializeCrons();
+  }
+
+  console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
+});
+
